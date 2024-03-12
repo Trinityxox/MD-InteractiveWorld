70773aa1fd24220f4b1a08502a90b88f47214b3e
Trinityxox <114918063+Trinityxox@users.noreply.github.com>
2024-03-12 09:58:21 +0000

    file changes

--- Assets/Scripts/Quit.cs
+++ Assets/Scripts/Quit.cs
@@ -7,7 +7,7 @@ public class Quit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello");
+
     }
 
     // Update is called once per frame
