c664271f85d8632a360097ca6c0db12cc439a59c
Trinityxox <114918063+Trinityxox@users.noreply.github.com>
2024-03-19 10:55:12 +0000

    C# code for button

---
+++ Assets/Scripts/Button1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button1 : MonoBehaviour
+{
+
+    public GameObject ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ui.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ui.SetActive(true);
+        }
+    }
+}
