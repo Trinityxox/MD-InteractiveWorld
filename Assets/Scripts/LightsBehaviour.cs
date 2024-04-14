e37f09dcd6d6f0415a33341391fb10a2820a0090
Trinityxox <114918063+Trinityxox@users.noreply.github.com>
2024-04-14 11:51:58 +0100

    lights code

--- Assets/Scripts/LightsBehaviour.cs
+++ Assets/Scripts/LightsBehaviour.cs
@@ -7,10 +7,13 @@ public class LightsBehaviour : MonoBehaviour
 
     public GameObject ui;
 
+    Light myLight;
+
     // Start is called before the first frame update
     void Start()
     {
         HideUI();
+        myLight = GetComponent<Light>();
     }
 
     // Update is called once per frame
@@ -28,10 +31,24 @@ public class LightsBehaviour : MonoBehaviour
 
 public void HideUI(){
 ui.SetActive(false);
+Cursor.lockState = CursorLockMode.Locked;
+Cursor.visible = false;
 }
 
 public void ShowUI(){
 ui.SetActive(true);
+Cursor.lockState = CursorLockMode.None;
+Cursor.visible = true;
+}
+
+public void PressYes(){
+    HideUI();
+    myLight.enabled = myLight.enabled;
+
+}
+
+public void PressNo(){
+    HideUI();
 }
 
 }
